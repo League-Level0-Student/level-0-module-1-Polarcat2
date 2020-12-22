4aa08c7a065655c70ec25a64b1f6a5233298f806
root <root@35e06bda001f>
2020-12-22 02:28:13 +0000

    Unbirthday.java

--- src/_02_colors/VennDiagram/VennDiagram.pde
+++ src/_02_colors/VennDiagram/VennDiagram.pde
@@ -1,1 +1,13 @@
 size(600,600);
+
+fill (#FF0303,100);
+ellipse (200,100,200,200);
+fill (#00FF2C,100);
+ellipse (200,200,200,200);
+fill (#0300FF,100);
+ellipse (300,100,200,100);
+textSize(45);
+fill(0);
+text("cats", 250, 120);
+text("cool", 130, 100);
+text("amazing", 90, 210);
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -1,18 +1,23 @@
 package _03_if_else._2_remarkable;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
 public class Remarkable {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+		String cat = "cool";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
-		// 3. In a pop-up, tell the user what is remarkable about that person. 
+		String name = JOptionPane.showInputDialog("What is your name?");
+		// 3. In a pop-up, tell the user what is remarkable about that person.
+		if (name.equalsIgnoreCase("cat")) {
+			JOptionPane.showMessageDialog(null, "You are really " + cat);
+		} else {
+			JOptionPane.showMessageDialog(null, "You suck.");
+		}
 
 	}
 }
-
