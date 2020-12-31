1e524f6b2e8ebb2a94f0c8ec8117b106a49ae39e
root <root@5ede64ff1437>
2020-12-31 00:28:41 +0000

    TastyTomato.pde

--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -19,16 +19,22 @@ public class SecretMessageBox {
 	// 0. Make a main method and put steps 1-5 inside it
 public static void main(String[] args) {
 	
-}
+
 	// 1. Set a password in a String variable
 	String password="1234";
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+	String message = JOptionPane.showInputDialog("give us the secret message.");
+		
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
+	String guess = JOptionPane.showInputDialog(null, "You can only see the secret message if you can guess the passcode");
 
 	// 4. If their guess matches the password, show them the secret message
-
+	if (password.equalsIgnoreCase(guess)) {
+	
+		JOptionPane.showMessageDialog(null, message);
+	}
 	// 5. If the password does not match, pop-up "INTRUDER!!"
-
-}
+	else {
+		JOptionPane.showMessageDialog(null, "INTRUDERRRRR!!!");
+}}}
--- src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_03_if_else/_4_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -4,7 +4,10 @@ void setup() {
 void draw() {
     background(200, 200, 200);
     noStroke();
+    fill(#930909);
     ellipse(150, 200, 150, 150);
     ellipse(212, 200, 150, 150);
+    fill(#054805);
     rect(176, 103, 12, 32);
+
 }
